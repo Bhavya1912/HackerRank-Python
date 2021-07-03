c496f91a5d0b24085bafb56dc59ecfedcd0e7e2f
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-07-03 13:01:48 +0530

    Create Capitalize.py

---
+++ Capitalize.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    x=""
+    l1=s.split(" ")
+    for i in l1:
+        x += i.capitalize()
+        x+= " "
+    return x
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
