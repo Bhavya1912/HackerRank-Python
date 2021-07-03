ef7e80f7b06f8c104068d3b50521dba0f9c7968c
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-07-03 13:01:03 +0530

    Create Alphabet Rangoli.py

---
+++ Alphabet Rangoli.py
@@ -0,0 +1,20 @@
+def print_rangoli(size):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    data=list()
+    for i in range(n):
+        data.append(alpha[i])
+    items = list(range(n))
+    items = items[:-1]+items[::-1]
+    for i in items:
+        temp = data[-(i+1):]
+        row = temp[::-1]+temp[1:]
+        row="-".join(row)
+        width= n*4-3
+        row=row.center(width, "-")
+        print(row)
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
