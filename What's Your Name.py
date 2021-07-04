c7d5f5c13959fe340ed6fb0178d550c6cfb4a2ef
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-07-05 01:26:38 +0530

    Add files via upload

--- What's Your Name.py
+++ What's Your Name.py
@@ -1,15 +1,5 @@
-#
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
-
-def print_full_name(first_name, last_name):
-    last_name=last_name+"!"
-    s="Hello "+first_name+" "+last_name+" You just delved into python."
+def print_full_name(a, b):
+    b=b+"!"
+    s="Hello "+a+" "+b+" You just delved into python."
     print(s)
 
-if __name__ == '__main__':
